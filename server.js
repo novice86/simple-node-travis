0f3f5529d3ef12931189ead963a52cb9819cbbfe
novice86 <dpopova10@gmail.com>
2021-03-12 17:18:46 -0800

    Add print for environment variable

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
+
+const thirdPlanet = process.env.THIRD_PLANET_FROM_SUN;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
+  console.log(`Third planet from the sun is ${thirdPlanet}` );
   while(true) {
     console.log('Containers rule!');
     await sleep(5000);
